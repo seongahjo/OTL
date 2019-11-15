84ec9b77b9d37dd9e0f217cf4c4e58c7fb23a52f
seongahjo <seongside@gmail.com>
2019-11-15 11:36:40 +0900

    Add Spring boot init (#5)

---
+++ server/src/test/java/com/sajo/hackday/server/HackdayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sajo.hackday.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackdayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
