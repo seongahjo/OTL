20df1ec89a5c84d0fc97bd809c6e57dd7ff62a51
seongahjo <seongside@gmail.com>
2019-11-21 19:27:00 +0900

    Add Attach Schedule Service to View Controller(#14)

--- server/src/main/java/com/otl/server/controller/ViewController.java
+++ server/src/main/java/com/otl/server/controller/ViewController.java
@@ -1,13 +1,19 @@
 package com.otl.server.controller;
 
+import com.otl.server.service.ScheduleService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class ViewController {
+	private final ScheduleService scheduleService;
+
 	@GetMapping("/")
 	public String mainView(Model model) {
+		model.addAttribute("schedules", scheduleService.getAllSchedules());
 		return "main";
 	}
 }
