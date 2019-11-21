35c0be8d6a17aa0993e3bd30004d8d4269f1fde4
seongahjo <seongside@gmail.com>
2019-11-21 20:00:58 +0900

    Add Init data(#15)

--- server/src/main/java/com/otl/server/controller/ViewController.java
+++ server/src/main/java/com/otl/server/controller/ViewController.java
@@ -2,18 +2,20 @@ package com.otl.server.controller;
 
 import com.otl.server.service.ScheduleService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @RequiredArgsConstructor
+@Slf4j
 public class ViewController {
 	private final ScheduleService scheduleService;
 
 	@GetMapping("/view")
 	public String mainView(Integer gameId, Model model) {
-		model.addAttribute(scheduleService.getScheduleByGameId(gameId));
+		model.addAttribute("schedule",scheduleService.getScheduleByGameId(gameId));
 		return "main";
 	}
 }
