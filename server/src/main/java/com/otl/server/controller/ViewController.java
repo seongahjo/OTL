03b4ea61d7b2b9e0f0263827ce3ed24041755239
seongahjo <seongside@gmail.com>
2019-11-21 17:41:27 +0900

    Add Controller to render main page (#15)

---
+++ server/src/main/java/com/otl/server/controller/ViewController.java
@@ -0,0 +1,13 @@
+package com.otl.server.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ViewController {
+	@GetMapping("/")
+	public String mainView(Model model){
+		return "main";
+	}
+}
