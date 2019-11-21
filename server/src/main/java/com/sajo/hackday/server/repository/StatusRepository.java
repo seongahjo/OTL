a204a46cb4e6b8b95df532f8a53991ca96fe1b5c
seongahjo <seongside@gmail.com>
2019-11-21 16:52:58 +0900

    Remove Temporary API (#7)

--- server/src/main/java/com/sajo/hackday/server/repository/StatusRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sajo.hackday.server.repository;
-
-import com.sajo.hackday.server.model.Status;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StatusRepository extends JpaRepository<Status,Long> {
-}
