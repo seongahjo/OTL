dd3e59c16761431f65c9e85f9b6b73ddc114cef3
Jason Huh <iriszero5@gmail.com>
2019-11-22 12:19:16 +0900

    Add MQ, Publish(#39)

--- push/index.js
+++ push/index.js
@@ -1,9 +1,38 @@
 const app = require('express')();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
-const axios = require('axios');
+const amqp = require('amqplib/channel_api');
 
 let map = new Map();
+let gameMap = new Map();
+gameMap.set("0", "https://nlive-hls.pstatic.net/ch100/ch100_2000.stream/playlist.m3u8?_lsu_sa_=6fa5b8e0433b3842b1139ee56f956bfa8a1961e37456812765a1ee224c54632119dd30036d85f5b216453d41d705619c2e9493d9c57bfd4d6be2f747307c849d010b8906285f19fe34f0adcb66ac08d9101789c4ffc6168c129c877f563087d9");
+gameMap.set("1", "https://nlive-hls.pstatic.net/ch21/ch21_2000.stream/playlist.m3u8?_lsu_sa_=64e510eac3fc31224d123e8c66e558f4ca3667433c57e1ef6241a520dc976d3141d3902c6135a5b266f73b41d109a178f67411ce8895da88325c5353c96bd0eea1d9f824ca793f7645a93d2c145d831cb6eb0c89d92acac5125a5eb909a09e8c");
+gameMap.set("2", "https://nlive-hls.pstatic.net/ch200/ch200_2000.stream/playlist.m3u8?_lsu_sa_=63a5f0e8d36634b2911b3e786b4525f50a856803835fe1946fb1c12a4c336fc1e8d1b0466e55d0bf466d3ca15d0b912d87651c7363af2b570dc4de43fac79703c9e5107fb986fc87543ed0caec606c5486448adcd0c372e7ffe6a4f730c4434a");
+let amqpUrl = "amqp://guest:guest@localhost";
+let q = 'OWL';
+
+const consumeToQueue = async (ch, queueName) => {
+    await ch.consume(queueName, (msg) => {
+        msgStr = msg.content.toString();
+        msgSplit = msgStr.split(';');
+        let clientId = msgSplit[0];
+        let gameId = msgSplit[1];
+        console.log(gameId);
+        let socket = map.get(clientId);
+        let gameUrl = gameMap.get(gameId);
+        console.log(gameUrl);
+        socket.emit('url', {url: gameUrl});
+        ch.ack(msg);
+    });
+};
+
+async function runMQ() {
+    conn = await amqp.connect(amqpUrl);
+    ch = await conn.createChannel();
+    await consumeToQueue(ch, q);
+}
+
+runMQ();
 
 server.listen(80);
 // WARNING: app.listen(80) will NOT work here!
@@ -16,24 +45,8 @@ app.get('/', function (req, res) {
 io.on('connection', function (socket) {
     socket.on('init', async function (data) {
         console.log(data);
-
-        var clientId = data.clientId;
-        var gameId = data.gameId;
-
+        var clientId = data.clientId+"";
+        var gameId = data.gameId+"";
+        map.set(clientId, socket);
     });
 });
-
-function fetchURL(clientId, gameId) {
-    axios.get(`http://106.10.34.82/play/${gameId}`)
-        .then(function (response) {
-            // handle success
-            var url = response.data.url;
-        })
-        .catch(function (error) {
-            // handle error
-            console.log(error);
-        })
-        .finally(function () {
-            // always executed
-        });
-}
