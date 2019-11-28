ee5ecfca51d07b0abe987bf22f0e59c1bd1ede78
seongahjo <seongside@gmail.com>
2019-11-28 20:53:45 +0900

    Add No Args Constructor to define bean (#3)

--- liveapi/src/main/java/com/otl/liveapi/model/PlayerId.java
+++ liveapi/src/main/java/com/otl/liveapi/model/PlayerId.java
@@ -2,11 +2,13 @@ package com.otl.liveapi.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class PlayerId implements Serializable {
 	protected long gameId;
 }
