6e475e92d4fd9f35bedf7fb879ff826a710ce1d3
seongahjo <seongside@gmail.com>
2019-11-29 04:13:03 +0900

    Add logging (#7)

--- push/index.js
+++ push/index.js
@@ -1,20 +1,36 @@
 const io = require('socket.io')(80);
 const amqp = require('amqplib/channel_api');
 const axios = require('axios');
-let liveAPI = 'http://localhost:8080';
-let map = new Map();
-let gameMap = new Map();
-// let amqpUrl = "amqp://rabbitmq:rabbitmq@10.41.0.142";
-let amqpUrl = "amqp://guest:guest@localhost";
-let q = 'OWL';
-let perRequestTime = 1;
+const config = require('./config.json');
+const map = new Map();
+const gameMap = new Map();
+const environment = process.env.NODE_ENV || 'development';
+const environmentConfig = config[environment];
+const liveAPI = environmentConfig.liveapi_url;
+const amqpUrl = environmentConfig.amqp_url;
+const perRequestTime = config.per_sec_request_time;
+const q = config.queue_name;
+const log = require('./config/winston');
+
+
+let conn;
+
+io.on('connection', function (socket) {
+	log.info(`socket connection is established`);
+	socket.on('init', async function (data) {
+		var clientId = data.clientId.toString();
+		// var gameId = data.gameId.toString();
+		log.info(`${clientId} is connected`);
+		map.set(clientId, socket);
+	});
+});
 
 async function connect() {
 	try {
 		conn = await amqp.connect(amqpUrl);
-		console.log('rabbit mq connected');
+		log.info(`rabbitmq ${amqpUrl} connected`);
 	} catch (err) {
-		console.log('not connected');
+		log.error(`rabbitmq ${amqpUrl} not connected ${err}`);
 	}
 }
 
@@ -22,30 +38,27 @@ const consumeToQueue = async (queueName) => {
 	if (conn === undefined) {
 		await connect();
 	}
-	ch = await conn.createChannel();
+	const ch = await conn.createChannel();
 	await ch.consume(queueName, (msg) => {
-		msgStr = Buffer.from(msg.content).toString();
-		msgSplit = msgStr.split(';');
-		console.log(msgStr);
-		let clientId = msgSplit[0];
-		let gameId = msgSplit[1];
-		console.log('queue : ' + clientId);
-		socket = map.get(clientId);
+		const msgStr = Buffer.from(msg.content).toString();
+		const msgSplit = msgStr.split(';');
+		log.info(`message "${msgStr}" is received from mq ${q}`);
+		const clientId = msgSplit[0];
+		const gameId = msgSplit[1];
+		const socket = map.get(clientId);
 		if (socket === undefined) {
-			console.log('not exists');
+			log.warn(`${clientId} is not connected`);
 			ch.nack(msg, false, true);
 			return;
 		}
 		map.delete(clientId);
-		let gameUrl = gameMap.get(gameId);
+		const gameUrl = gameMap.get(gameId);
 		socket.emit('url', {url: gameUrl});
-		console.log('message consume');
+		log.info(`${gameUrl} is sent to ${clientId}`);
 		ch.ack(msg);
 	});
 	await ch.close();
 };
-
-let conn;
 
 (async function init() {
 	await connect();
@@ -54,11 +67,12 @@ let conn;
 
 async function request() {
 	try {
-		console.log('request');
-		let res = await axios.get(liveAPI + "/play");
-		// await consumeToQueue(q);
-		let isCongested = res.data.isCongested;
-		let plays = res.data.plays;
+		log.info(`fetch request ${liveAPI}/play`);
+		const res = await axios.get(liveAPI + "/play");
+		const isCongested = res.data.congested;
+		log.info(`now congestion is ${isCongested}`);
+		const plays = res.data.plays;
+		log.info(`url is received ${plays}`);
 		for (let play of plays) {
 			gameMap.set(play.gameId, play.url);
 		}
@@ -66,19 +80,9 @@ async function request() {
 			await consumeToQueue(q);
 		}
 	} catch (err) {
-		console.error(err);
-		console.log('error');
+		log.error(`server ${liveAPI} is down ${err}`);
 	} finally {
 		setTimeout(request, perRequestTime * 1000 * 60);
 	}
 }
 
-
-io.on('connection', function (socket) {
-	socket.on('init', async function (data) {
-		var clientId = data.clientId.toString();
-		// var gameId = data.gameId.toString();
-		console.log('connection ' + clientId);
-		map.set(clientId, socket);
-	});
-});
