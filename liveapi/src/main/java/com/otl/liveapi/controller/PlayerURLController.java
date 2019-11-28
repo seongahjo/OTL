f1e4ded58728058acd2d0bbf36cb50df9d65d128
seongahjo <seongside@gmail.com>
2019-11-28 16:45:24 +0900

    Add ResponseEntity Wrapping (#3)

--- liveapi/src/main/java/com/otl/liveapi/controller/PlayerURLController.java
+++ liveapi/src/main/java/com/otl/liveapi/controller/PlayerURLController.java
@@ -5,6 +5,7 @@ import com.otl.liveapi.service.PlayerURLService;
 import com.otl.liveapi.service.ServerStatusService;
 import com.otl.liveapi.vo.PlayResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,10 +17,10 @@ public class PlayerURLController {
 	private final ServerStatusService serverStatusService;
 
 	@GetMapping("/play/{game_id}")
-	public PlayResponse getPlayerURL(@PathVariable("game_id") long gameId) {
+	public ResponseEntity<PlayResponse> getPlayerURL(@PathVariable("game_id") long gameId) {
 		boolean isCongested = serverStatusService.getIsCongested();
 		if (isCongested)
-			return new PlayResponse(false);
-		return new PlayResponse(true, gameId, playerURLService.getURLById(gameId));
+			return ResponseEntity.ok(new PlayResponse(false));
+		return ResponseEntity.ok(new PlayResponse(true, gameId, playerURLService.getURLById(gameId)));
 	}
 }
